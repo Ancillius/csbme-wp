41d1f87931b1f70177b4b263d56f679a34c78f9f
Ancillius <nils.schasse@gmail.com>
2015-07-11 12:53:50 +0200

    Entferne Style Scope

--- BerufeConv.php
+++ BerufeConv.php
@@ -40,11 +40,6 @@ class BerufeConv {
     public function renderHtml() {
         ob_start();
         ?>
-        <style type="text/css">
-            #MyTabContent {
-
-            }
-        </style>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
         <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 
